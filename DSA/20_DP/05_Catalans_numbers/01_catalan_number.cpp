8ec23115742c432fc5588e864a4263b31a8b4f64
Krushna Baravkar <krushnabaravkar38@gmail.com>
2026-06-05 20:24:51 +0530

    Catalan's number problems

--- DSA/20_DP/05_Catalans_numbers/01_catalan_number.cpp
+++ DSA/20_DP/05_Catalans_numbers/01_catalan_number.cpp
@@ -13,7 +13,35 @@ int catlan(int n){
 
     return ans;
 }
+
+// memoization approch.
+int catlan_memoization(int n, vector<int> &dp){
+    if(n == 0 || n == 1) return 1;
+    if(dp[n] != -1) return dp[n];
+
+    int ans = 0;  // variable to store Nth cal=talan.
+    for(int i=0; i<n; i++){
+        ans += (catlan_memoization(i, dp) * catlan_memoization(n-i-1, dp));
+    }
+
+    return dp[n] = ans;
+}
+
+// tabulation approch.
+int catlan_tabulation(int n){
+    vector<int> dp(n+1, 1);
+
+    for(int i=2; i<=n; i++){
+        int ans = 0;
+        for(int j=0; j<i; j++){
+            ans += dp[j] * dp[i-j-1];
+        }
+        dp[i] = ans;
+    }
+    return dp[n];
+}
+
 int main() {
-    cout<<catlan(3);
+    cout<<catlan_tabulation(3);
     return 0;
 }
