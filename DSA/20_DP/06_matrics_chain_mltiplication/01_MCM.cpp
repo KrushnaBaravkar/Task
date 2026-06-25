df5b4b3652a33ac68f93124f5e1f9b1f35ad2140
Krushna Baravkar <krushnabaravkar38@gmail.com>
2026-06-25 10:50:12 +0530

    memeization approch for MCM

--- DSA/20_DP/06_matrics_chain_mltiplication/01_MCM.cpp
+++ DSA/20_DP/06_matrics_chain_mltiplication/01_MCM.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-int MCM(vector<int> vec, int i, int j){ // return minimum cost to multiply j metrics
+
+// recurssive approch
+int MCM(vector<int> &vec, int i, int j){ // return minimum cost to multiply j metrics
     if(i == j){
         return 0;
     }    
@@ -19,10 +21,45 @@ int MCM(vector<int> vec, int i, int j){ // return minimum cost to multiply j met
     return ans;
 }
 
+// Mamoization approch 
+// carefully understand the creation of the storage and its size and how does it get used in the code is aslo important.
+int MCM_mamo(const vector<int>& vec,
+             int i,
+             int j,
+             vector<vector<int>>& dp)
+{
+    if(i == j){
+        return dp[i-1][j-1] = 0;
+    }
+
+    if(dp[i-1][j-1] != -1){
+        return dp[i-1][j-1];
+    }
+
+    int ans = INT_MAX;
+
+    for(int k = i; k < j; k++){
+
+        int cost1 = MCM_mamo(vec, i, k, dp);
+
+        int cost2 = MCM_mamo(vec, k + 1, j, dp);
+
+        int curr_cost =
+            cost1 +
+            cost2 +
+            vec[i - 1] * vec[k] * vec[j];
+
+        ans = min(ans, curr_cost);
+    }
+
+    return dp[i-1][j-1] = ans;
+}
+
 int main() {
     vector<int> a = {1,2,3,4,3};
     int n = a.size();
+    vector<vector<int>> dp(n-1, vector<int>(n-1, -1));
 
-    cout<<"Minimum poertion code for the given matrics is "<<MCM(a, 1, n-1);  // for n elements in the vector there are only n-1 matrics will form in present question or case.
+    cout<<"Minimum poertion code for the given matrics is "<<MCM_mamo(a, 1, n-1, dp);  // for n elements in the vector there are only n-1 matrics will form in present question or case.
     return 0;
 }
