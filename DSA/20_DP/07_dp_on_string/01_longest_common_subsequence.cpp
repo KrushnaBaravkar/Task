1c6b25d3a98b41f0fb80ae58d39b5c8e1113a7f0
Krushna Baravkar <krushnabaravkar38@gmail.com>
2026-08-10 17:27:03 +0530

    asfd

--- DSA/20_DP/07_dp_on_string/01_longest_common_subsequence.cpp
+++ DSA/20_DP/07_dp_on_string/01_longest_common_subsequence.cpp
@@ -20,14 +20,39 @@ int LCS(string &s1, string &s2, int i, int j) {
     return max(ans1, ans2);
 }
 
+// DP solution(Predered)
+void dpLCS(string s1, string s2, vector<vector<int>> &dp){
+    int n = s1.size();
+    int m = s2.size();
+
+    for(int i = 0; i < max(n, m); i++){
+        if(i<n){
+            dp[i][0] = 0;
+        }
+        if(i<m){
+            dp[0][i] = 0;
+        }
+    }
+
+    for(int i =1; i<=n; i++){
+        for(int j = 1; j<=m; j++){
+            if(s1[i-1] == s2[j-1]){
+                dp[i][j] = dp[i-1][j-1] + 1;
+            }
+            else{
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+    }
+}
+
 int main() {
 
     string s1 = "avedgsr";
     string s2 = "aedhjsien";
 
-    cout << "LCS length = "
-         << LCS(s1, s2, 0, 0)
-         << endl;
-
+    vector<vector<int>>dp(s1.size()+1, vector<int>(s2.size()+1, -1));
+    dpLCS(s1, s2, dp);
+    cout << dp[s1.size()][s2.size()] << endl;
     return 0;
 }
--- DSA/20_DP/07_dp_on_string/02_longest_subsequence.cpp
+++ DSA/20_DP/07_dp_on_string/02_longest_subsequence.cpp
@@ -21,6 +21,45 @@ string LCS(string &s1, string &s2, int i, int j) {
     return (ans1.size() > ans2.size()) ? ans1 : ans2;
 }
 
+// using DP
+// DP / Tabulation solution
+void dpLCS(string s1, string s2, vector<vector<string>> &dp) {
+
+    int n = s1.size();
+    int m = s2.size();
+
+    // Base cases
+    for(int i = 0; i <= n; i++) {
+        dp[i][0] = "";
+    }
+
+    for(int j = 0; j <= m; j++) {
+        dp[0][j] = "";
+    }
+
+    // Fill DP table
+    for(int i = 1; i <= n; i++) {
+
+        for(int j = 1; j <= m; j++) {
+
+            // Characters match
+            if(s1[i-1] == s2[j-1]) {
+                dp[i][j] = dp[i-1][j-1] + s1[i-1];
+            }
+
+            // Characters don't match
+            else {
+                if(dp[i-1][j].size() > dp[i][j-1].size()) {
+                    dp[i][j] = dp[i-1][j];
+                }
+                else {
+                    dp[i][j] = dp[i][j-1];
+                }
+            }
+        }
+    }
+}
+
 int main() {
 
     string s1 = "avedgsr";
